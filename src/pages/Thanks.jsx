4c3ae354ff6e7862383e09b6e3b145fbd8535891
Justcallme-H <akritmhj7@gmail.com>
2023-04-02 19:58:42 +0545

    add link to thanks

--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { backCard, frontCard, gradient, mobileGradient, logo, complete } from '../assets/images'; 
+import { Link, useNavigate } from 'react-router-dom';
 
 const Thanks = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted!")
+    navigate("/");
+  }
+
   const isSmall = window.matchMedia('(max-width: 399px)').matches;
   return (
     <section className="flex mobile:flex-row flex-col">
@@ -23,7 +31,9 @@ const Thanks = () => {
           <img src={complete} className="" />
           <h1 className="text-3xl font-normal mt-8">Thank You!</h1>
           <p className="text-lg font-normal mt-3">We've added your card details</p>
-          <input type="submit" className="w-[379px] h-[53px] bg-black text-white mobile:mt-4 mt-8 rounded-lg transition duration-150 ease-in-out hover:scale-105 hover:drop-shadow-lg cursor-pointer" placeholder='Submit' />
+          <Link to="/">
+            <button type="submit" className="mobile:w-[379px] w-[315px] h-[53px] bg-black text-white mobile:mt-4 mt-6 rounded-lg transition duration-150 ease-in-out hover:scale-105 hover:drop-shadow-lg cursor-pointer">Continue</button>
+          </Link>
         </div>
       </div>
     </section>
